0cfb418488b4ca90463cf3e0a51ae1209f9a0f98
zheng7641 <zheng7641@163.com>
2016-12-27 18:01:51 +0800

    2016-12-27
    准备做restfull

--- src/main/java/com/zheng/index/IndexController.java
+++ src/main/java/com/zheng/index/IndexController.java
@@ -6,15 +6,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/index")
-	public String index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	
+	private static final Logger logger = Logger.getLogger(IndexController.class);
+	@RequestMapping("{http}")
+	public String index(@PathVariable("http")String http,HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 //		request.getRequestDispatcher("/WEB-INF/view/index.html").forward(request, response);
+		logger.info(http);
 		return "index";
 	}
 }
